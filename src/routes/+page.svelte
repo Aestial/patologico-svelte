424984db670e5d7296f05ffbc24f8c8e27e44402
Jaime Hernandez <jaimevazz@gmail.com>
2023-12-10 15:42:30 -0500

    Circular color list for shirt using swipe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,52 @@
-<script>
+<script lang="ts">
 	import Patologico from './Patologico.svelte';
 	import BurgerMenu from './BurgerMenu.svelte';
 	import Swiper from './Swiper.svelte';
+	import colorLib from '$lib/json/colors.json';
+	import { onMount } from 'svelte';
 
 	const colors = {
-		pattern: "#d45500",
-		shirt: "#7ea889",
-		tie: "#690000"
+		pattern: '#d45500',
+		shirt: '#7ea889',
+		tie: '#690000'
 	};
-	
-	function updateColor(event) {		
+
+	const indices = {
+		pattern: getRandomInt(colorLib.tie.length),
+		shirt: getRandomInt(colorLib.shirt.length),
+		tie: getRandomInt(colorLib.tie.length)
+	};
+
+	onMount(() => {
+		colors.pattern = colorLib.tie[indices.pattern];
+		colors.shirt = colorLib.shirt[indices.shirt];
+		colors.tie = colorLib.tie[indices.tie];
+	});
+
+	function updateColor(event: any) {
 		switch (event.detail) {
 			case 'left':
 				console.log('Back');
-				colors.pattern = "#fff";
+				colors.shirt = selectFromArray(colorLib.shirt, false);
 				break;
 			case 'right':
 				console.log('Next');
-				colors.pattern = "#000";
+				colors.shirt = selectFromArray(colorLib.shirt, true);
 				break;
 		}
+	}
+
+	function selectFromArray(options: string[], forward: boolean = true): string {
+		if (forward) {
+			if (indices.shirt++ >= options.length - 1) indices.shirt = 0;
+		} else {
+			if (indices.shirt-- <= 0) indices.shirt = options.length - 1;
+		}
+		return options[indices.shirt];
+	}
+
+	function getRandomInt(max: number) {
+		return Math.floor(Math.random() * max);
 	}
 </script>
 
@@ -29,7 +56,7 @@
 </svelte:head>
 
 <section>
-	<Swiper on:swipe={updateColor}/>
+	<Swiper on:swipe={updateColor} />
 	<Patologico {...colors} />
 	<BurgerMenu />
 </section>
