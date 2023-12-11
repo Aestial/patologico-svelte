e0f6c6f15e31452249bcf858c2ade8f3d8a964a1
Jaime Hernandez <jaimevazz@gmail.com>
2023-12-11 01:16:49 -0500

    Better BurgerMenu component

--- src/routes/BurgerMenu.svelte
+++ src/routes/BurgerMenu.svelte
@@ -1,43 +1,105 @@
 <script lang="ts">
-	import die_icon from '$lib/svg/die3d.svg';
-	import plus_icon from '$lib/svg/plus.svg';
+	import brush from '$lib/svg/brush.svg';
+	import burger from '$lib/svg/burger.svg';
+	import diamond from '$lib/svg/diamond.svg';
+	import die from '$lib/svg/die-icon.svg';
+	import shirt from '$lib/svg/shirt.svg';
+	import tie from '$lib/svg/tie.svg';
+
+	enum Tool {
+		brush,
+		diamond,
+		shirt,
+		tie
+	};
 
 	let expanded = false;
-
-	const focusFirstDropdownLink = ({ target }) => {
-		target.firstElementChild.focus();
-	};
+	let tool: Tool = Tool.brush;
 
 	function reload() {
 		location.reload();
-	}
+	};
+
+	function setBrush(){
+		setTool(Tool.brush);
+	};
+	function setDiamond(){
+		setTool(Tool.diamond);
+	};
+	function setShirt(){
+		setTool(Tool.shirt);
+	};
+	function setTie(){
+		setTool(Tool.tie);
+	};
+
+	function setTool(value: Tool) {
+		tool = value;
+		console.log(tool);
+	};
+
 </script>
 
 <nav>
-	<button
+	<div
 		class="toggle-dropdown"
+		role="button"
+		tabindex="0"
 		aria-expanded={expanded}
 		aria-haspopup="true"
 		aria-label="Burger toggle"
 		on:click={() => (expanded = !expanded)}
+		on:keydown={() => (expanded = !expanded)}
 	>
-		<svg viewBox="0 0 448 512" width="120" title="bars">
+		<svg viewBox="0 0 420 460" width="120" title="bars">
 			<path
+				style="opacity:0.50000000000000000;fill:#faf5f1;fill-opacity:1;fill-rule:nonzero;stroke:none"
 				d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
 			/>
 		</svg>
-	</button>
+	</div>
 	<!--Note we can use class:expanded to bind our "expanded" CSS class to the "expanded" state variable-->
-	<div class="dropdown" class:expanded on:transitionend={focusFirstDropdownLink}>
+	<!-- <div class="dropdown" class:expanded on:transitionend={focusFirstDropdownLink}> -->
+	<div class="dropdown" class:expanded>
 		<div
 			role="button"
 			tabindex="0"
-			on:click={reload}
-			on:keydown={reload}
+			on:click={setShirt}
+			on:keydown={setShirt}
 			aria-label="PANIC!"
 			class="menu-button"
 		>
-			<img src={plus_icon} alt="Plus button" />
+			<img src={shirt} alt="Plus button" />
+		</div>
+		<div
+			role="button"
+			tabindex="0"
+			on:click={setTie}
+			on:keydown={setTie}
+			aria-label="PANIC!"
+			class="menu-button"
+		>
+			<img src={tie} alt="Plus button" />
+		</div>
+		<div
+			role="button"
+			tabindex="0"
+			on:click={setDiamond}
+			on:keydown={setDiamond}
+			aria-label="PANIC!"
+			class="menu-button"
+		>
+			<img src={diamond} alt="Plus button" />
+		</div>
+		<div
+			role="button"
+			tabindex="0"
+			on:click={setBrush}
+			on:keydown={setBrush}
+			aria-label="PANIC!"
+			class="menu-button"
+		>
+			<img src={brush} alt="Plus button" />
 		</div>
 		<div
 			role="button"
@@ -47,7 +109,7 @@
 			aria-label="PANIC!"
 			class="menu-button"
 		>
-			<img src={die_icon} alt="Die button" />
+			<img src={die} alt="Die button" />
 		</div>
 	</div>
 </nav>
@@ -57,15 +119,6 @@
 		position: fixed;
 		left: 20px;
 		bottom: 20px;
-	}
-
-	.menu-button {
-		margin-top: 10px;
-	}
-
-	.menu-button img {
-		width: 76px;
-		height: 76px;
 	}
 
 	@media (max-width: 800px) {
@@ -97,15 +150,25 @@
 		left: 0;
 	}
 
-	.toggle-dropdown svg {
-		width: 58px;
-		height: 36px;
-		vertical-align: middle;
+	.menu-button {
+		margin-top: 10px;
+	}
+
+	.menu-button img {
+		width: 76px;
+		height: 76px;
 	}
 
 	.toggle-dropdown {
+		margin-bottom: 10px;
+		text-align: center;
 		z-index: 1;
 		cursor: pointer;
 	}
 
+	.toggle-dropdown svg {
+		width: 66px;
+		height: 48px;
+		vertical-align: middle;
+	}
 </style>
