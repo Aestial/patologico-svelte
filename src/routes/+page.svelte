df8899379141026efd155b977bab204d5de0ba9c
Jaime Hernandez <jaimevazz@gmail.com>
2023-12-12 07:44:53 -0500

    Create ColorPick component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,26 @@
 <script lang="ts">
-	import Patologico from './Patologico.svelte';
+	import options from '$lib/json/colors.json';
+
 	import BurgerMenu from './BurgerMenu.svelte';
+	import ColorPick from './ColorPick.svelte';
 	import PagerInput from './PagerInput.svelte';
-	import colorLib from '$lib/json/colors.json';
-	import { onMount } from 'svelte';
+	import Patologico from './Patologico.svelte';
 
-	const patoColors = {
-		pattern: '#d45500',
-		shirt: '#7ea889',
-		tie: '#690000'
-	};
-
-	let bgColor = '#cedce8';
-
-	const indices = {
-		background: Math.randomInt(colorLib.background.length),
-		pattern: Math.randomInt(colorLib.tie.length),
-		shirt: Math.randomInt(colorLib.shirt.length),
-		tie: Math.randomInt(colorLib.tie.length)
-	};
-
-	onMount(() => {
-		bgColor = colorLib.background[indices.background];
-		patoColors.pattern = colorLib.tie[indices.pattern];
-		patoColors.shirt = colorLib.shirt[indices.shirt];
-		patoColors.tie = colorLib.tie[indices.tie];
-	});
+	const colors = {
+		bg: "#fff",
+		pato: {
+			pattern: "#fff",
+			shirt: "#fff",
+			tie: "#fff"
+		}
+	}
 
 	function nextColor(event: any) {
-		console.log("Next");
-		patoColors.pattern = selectFromArray(colorLib.tie, true);
-		patoColors.shirt = selectFromArray(colorLib.shirt, true);
+		console.log('Next');
 	}
 
 	function previousColor(event: any) {
-		console.log("Back");
-		patoColors.pattern = selectFromArray(colorLib.tie, false);
-		patoColors.shirt = selectFromArray(colorLib.shirt, false);
-	}
-
-	function selectFromArray(options: string[], forward: boolean = true): string {
-		if (forward) {
-			if (++indices.shirt >= options.length) indices.shirt = 0;
-		} else {
-			if (--indices.shirt < 0) indices.shirt = options.length - 1;
-		}
-		return options[indices.shirt];
+		console.log('Back');
 	}
 </script>
 
@@ -54,10 +29,14 @@
 	<meta name="description" content="Patologico ultra hi-res character" />
 </svelte:head>
 
-<section style:--bg={bgColor}>
+<section style:--bg={colors.bg}>
 	<PagerInput on:back={previousColor} on:next={nextColor} />
-	<Patologico {...patoColors} />
+	<Patologico {...colors.pato} />
 	<BurgerMenu />
+	<ColorPick bind:value={colors.bg} options={options.background} />
+	<ColorPick bind:value={colors.pato.pattern} options={options.tie} />
+	<ColorPick bind:value={colors.pato.shirt} options={options.shirt} />
+	<ColorPick bind:value={colors.pato.tie} options={options.tie} />
 </section>
 
 <style>
---
+++ src/routes/ColorPick.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let options: string[];
+    export let value = '#fff';
+	
+    const length = options.length;
+
+	$: index = Math.randomInt(length);
+	$: if (index >= length) index = 0;
+	$: if (index < 0) index = length - 1;
+
+	$: value = options[index];
+</script>
